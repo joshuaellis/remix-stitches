ecc466bddabb15120aac229832a6960e8d235cef
Josh <37798644+joshuaellis@users.noreply.github.com>
2021-12-27 17:47:29 +0000

    Initial commit

---
+++ app/root.tsx
@@ -0,0 +1,51 @@
+import {
+  Links,
+  LiveReload,
+  Meta,
+  Outlet,
+  Scripts,
+  ScrollRestoration,
+} from "remix";
+import type { MetaFunction } from "remix";
+
+import { getCssText } from "./styles/stitches.config";
+import { globalStyles } from "./styles/global";
+
+export const meta: MetaFunction = () => {
+  return {
+    title: "react-spring",
+    description:
+      "Bring your components to life with simple spring animation primitives for React",
+  };
+};
+
+function Document({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="en">
+      <head>
+        <meta charSet="utf-8" />
+        <Meta />
+        <Links />
+        <style
+          id="stitches"
+          suppressHydrationWarning
+          dangerouslySetInnerHTML={{ __html: getCssText() }}
+        />
+      </head>
+      <body>
+        {children}
+        <Scripts />
+        {process.env.NODE_ENV === "development" && <LiveReload />}
+      </body>
+    </html>
+  );
+}
+
+export default function App() {
+  return (
+    <Document>
+      <Outlet />
+      <ScrollRestoration />
+    </Document>
+  );
+}
